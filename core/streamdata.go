3b77e66239d9d03177ad166fa734d502b2410477
Angie Ramirez <angie@livepeer.org>
2018-11-29 13:17:21 -0500

    moving SegmentMetadata to streamdata

--- core/streamdata.go
+++ core/streamdata.go
@@ -4,8 +4,16 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"math/big"
 	"math/rand"
 	"time"
+
+	ethcommon "github.com/ethereum/go-ethereum/common"
+
+	"github.com/livepeer/go-livepeer/common"
+	"github.com/livepeer/go-livepeer/net"
+
+	"github.com/livepeer/lpms/ffmpeg"
 )
 
 var ErrStreamID = errors.New("ErrStreamID")
@@ -14,6 +22,26 @@ var ErrManifestID = errors.New("ErrManifestID")
 const (
 	HashLength = 32
 )
+
+type SegmentMetadata struct {
+	ManifestID ManifestID
+	Seq        int64
+	Hash       ethcommon.Hash
+	Profiles   []ffmpeg.VideoProfile
+	OS         *net.OSInfo
+}
+
+func (s *SegmentMetadata) Flatten() []byte {
+	profiles := common.ProfilesToHex(s.Profiles)
+	seq := big.NewInt(s.Seq).Bytes()
+	buf := make([]byte, len(s.ManifestID)+32+len(s.Hash.Bytes())+len(profiles))
+	i := copy(buf[0:], []byte(s.ManifestID))
+	i += copy(buf[i:], ethcommon.LeftPadBytes(seq, 32))
+	i += copy(buf[i:], s.Hash.Bytes())
+	i += copy(buf[i:], []byte(profiles))
+	// i += copy(buf[i:], []byte(s.OS))
+	return buf
+}
 
 func RandomVideoID() []byte {
 	rand.Seed(time.Now().UnixNano())
