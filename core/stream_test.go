52c113105099abbd6f8510a4a623eab3b8c65b9a
Angie Ramirez <angie@livepeer.org>
2018-11-30 11:44:02 -0500

    transcodeConfig clean up, profiles hardcodes remove

--- core/stream_test.go
+++ core/stream_test.go
@@ -10,14 +10,14 @@ import (
 )
 
 func TestSegmentFlatten(t *testing.T) {
-	s := SegmentMetadata{
+	md := SegmentMetadata{
 		ManifestID: ManifestID("abcdef"),
 		Seq:        1234,
 		Hash:       ethcommon.BytesToHash(ethcommon.RightPadBytes([]byte("browns"), 32)),
 		Profiles:   []ffmpeg.VideoProfile{ffmpeg.P144p30fps16x9, ffmpeg.P240p30fps16x9},
 	}
 	sHash := ethcommon.FromHex("e97461de03dcb5bf7f2e95c4ca9c99db2d049fb18a6df67dd9d557b2c05f6473")
-	if !bytes.Equal(ethcrypto.Keccak256(s.Flatten()), sHash) {
+	if !bytes.Equal(ethcrypto.Keccak256(md.Flatten()), sHash) {
 		t.Error("Flattened segment + hash did not match expected hash")
 	}
 }
